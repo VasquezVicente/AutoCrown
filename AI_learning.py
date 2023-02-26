4b5c9efe31e17a07292a67891f3b54434109b6f6
P_polycephalum <vicenteavv7@gmail.com>
2023-02-26 16:50:02 -0500

    finalizing introduction code and exploring tensorflow

--- AI_learning.py
+++ AI_learning.py
@@ -1,6 +1,7 @@
 #Numpy 
 ##dot product 
 import numpy as np 
+import matplotlib.pyplot as plt
 ## linear regression with numpy 
 input_vector = [1.72, 1.23]
 weights_1 = [1.26, 0]
@@ -15,6 +16,146 @@ dot_product_1 = np.dot(input_vector, weights_1)
 print(f"The dot product is: {dot_product_1}")
 dot_product_2 = np.dot(input_vector, weights_2)
 print(f"The dot product is: {dot_product_2}")
+
+# Wrapping the vectors in NumPy arrays
+input_vector = np.array([1.66, 1.56])
+weights_1 = np.array([1.45, -0.66])
+bias = np.array([0.0])
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def make_prediction(input_vector, weights, bias):
+    layer_1 = np.dot(input_vector, weights) + bias
+    layer_2 = sigmoid(layer_1)
+    return layer_2  
+
+prediction = make_prediction(input_vector, weights_1, bias)
+print(f"The prediction result is: {prediction}")
+
+target = 0
+mse = np.square(prediction - target)
+print(f"Prediction: {prediction}; Error: {mse}")
+
+derivative= 2*(prediction -target)
+weights_1= weights_1 -derivative
+prediction=make_prediction(input_vector, weights_1,bias )
+
+error= (prediction -target)**2
+print(f"Prediction: {prediction}; Error: {error}")
+
+def sigmoid_deriv(x):
+ return sigmoid(x) * (1-sigmoid(x))
+
+derror_dprediction = 2 * (prediction - target)
+layer_1 = np.dot(input_vector, weights_1) + bias
+dprediction_dlayer1 = sigmoid_deriv(layer_1)
+dlayer1_dbias = 1
+
+derror_dweights = (derror_dprediction * dprediction_dlayer1 * dlayer1_dbias)
+
+
+##neural network class
+class NeuralNetwork:
+    def __init__(self, learning_rate):
+        self.weights = np.array([np.random.randn(), np.random.randn()])
+        self.bias = np.random.randn()
+        self.learning_rate = learning_rate
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def _sigmoid_deriv(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
+
+    def predict(self, input_vector):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+        return prediction
+
+    def _compute_gradients(self, input_vector, target):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+
+        derror_dprediction = 2 * (prediction - target)
+        dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
+        dlayer1_dbias = 1
+        dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
+
+        derror_dbias = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+        )
+        derror_dweights = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
+        )
+
+        return derror_dbias, derror_dweights
+
+    def _update_parameters(self, derror_dbias, derror_dweights):
+        self.bias = self.bias - (derror_dbias * self.learning_rate)
+        self.weights = self.weights - (
+            derror_dweights * self.learning_rate
+        )
+
+    def train(self, input_vectors, targets, iterations):
+        cumulative_errors = []
+        for current_iteration in range(iterations):
+            # Pick a data instance at random
+            random_data_index = np.random.randint(len(input_vectors))
+            input_vector = input_vectors[random_data_index]
+            target = targets[random_data_index]
+            # Compute the gradients and update the weights
+            derror_dbias, derror_dweights = self._compute_gradients(
+                input_vector, target
+            )
+            self._update_parameters(derror_dbias, derror_dweights)
+            # Measure the cumulative error for all the instances
+            if current_iteration % 100 == 0:
+                cumulative_error = 0
+                # Loop through all the instances to measure the error
+                for data_instance_index in range(len(input_vectors)):
+                    data_point = input_vectors[data_instance_index]
+                    target = targets[data_instance_index]
+                    prediction = self.predict(data_point)
+                    error = np.square(prediction - target)
+                    cumulative_error = cumulative_error + error
+                cumulative_errors.append(cumulative_error)
+        return cumulative_errors
+
+
+##predictions
+learning_rate= 0.1
+neural= NeuralNetwork(learning_rate)
+neural.predict(input_vector)
+
+input_vectors = np.array(
+[
+[3, 1.5],
+[2, 1],
+[4, 1.5],
+[3, 4],
+[3.5, 0.5],
+[2, 0.5],
+[5.5, 1],
+[1, 1],
+]
+)
+
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+learning_rate = 0.1
+neural = NeuralNetwork(learning_rate)
+training_error = neural.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
+
+neural.predict([0.1,1.2])
+
+
 
 
 
--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 Automatic crown segmentation of trees from aerial RGB photos.
 I will be starting with 3 tif images from the 50 ha plot in BCI from the beggining of 2022
 All aerial images will be saved in aerial_imagery, sub-folder site 
-How to build an AI based on crown imagery 
-explore tensorflow and pytorch
+
+How to build an AI based on crown imagery?
+    #numpy to work with arrays, AI learning shows a neural network to predict binary 0,1 with simund distribution
+
+Explore tensorflow
+    #tensorflow google tool publicaly avaliable
+    image recognizing with keras package
+     eed deliniation tools, images are too big to learn consider smaller images 
+     pooling and convolution
+     
+literature review links
+    https://www.mdpi.com/2072-4292/12/15/2426
+
